fff3ac7ceda0218c1bb4d2f14723e8520f5f9463
Jin Kun Chai <jk.chai@hostbridge.com>
2022-04-05 12:36:42 -0500

    update express.js sample code

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+const Joi = require("joi");
 const express = require("express");
 
+// initialize
 const app = express();
+
+// adding piece of middleware
+// add to the request response
+app.use(express.json());
 
 // http verb/method
 /*
@@ -25,23 +31,96 @@ app.get("/api/courses", (req, res) => {
   res.send(courses);
 });
 
-app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course)
-    res.status(404).send("The course with the given ID was not found!");
+// Create
+app.post("/api/courses", (req, res) => {
+  // input validation
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  const result = schema.validate(req.body);
+  console.log(result);
+
+  // if (!req.body.name || req.body.name.length < 3) {
+  //   // 400 bad request
+  //   res.status(400).send("Name is required and should be minimum 3 characters");
+  //   return;
+  // }
+
+  // with JOI the above can be converted to the below
+  if (result.error) {
+    // 400 bad request
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
   res.send(course);
 });
 
-// colon indicates parameter for necessary
-app.get("/api/course/:year/:month", (req, res) => {
-  res.send(req.params);
+// handling update method
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given ID was not found!");
+
+  // input validation
+  // const result = validateCourse(req.body); //this can be written in destructive manner
+  const { error } = validateCourse(req.body);
+
+  // with JOI the above can be converted to the below
+  if (error) {
+    // 400 bad request
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  course.name = req.body.name;
+  res.send(course);
 });
 
-// query parameter for optional objects
-// localhost:3090/api/course/2022/4?sortBY=Name
-app.get("/api/course/:year/:month", (req, res) => {
-  res.send(req.query);
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(course);
+}
+
+// delete method
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given id was not valid!");
+
+  // find index
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
 });
+
+// app.get("/api/courses/:id", (req, res) => {
+//   const course = courses.find((c) => c.id === parseInt(req.params.id));
+//   if (!course)
+//     res.status(404).send("The course with the given ID was not found!");
+//   res.send(course);
+// });
+
+// // colon indicates parameter for necessary
+// app.get("/api/course/:year/:month", (req, res) => {
+//   res.send(req.params);
+// });
+
+// // query parameter for optional objects
+// // localhost:3090/api/course/2022/4?sortBY=Name
+// app.get("/api/course/:year/:month", (req, res) => {
+//   res.send(req.query);
+// });
 
 port = process.env.PORT || 3090;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
